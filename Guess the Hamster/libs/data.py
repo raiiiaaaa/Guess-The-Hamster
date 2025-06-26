09bdf7f3f3231a07a56b26fbdafcc825c2cb883f
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-06-26 12:56:02 +0700

    Add files via upload

---
+++ Guess the Hamster/libs/data.py
@@ -0,0 +1,11 @@
+# libs/data.py
+
+game_status = {
+    'username': None,
+    "koin": 0,
+    "auto_catch": 0,
+    "double_point": 0,
+    "fifty_fifty": 0,
+    "is_developer": False,
+    "win_event": False
+}
