29ba6f25748a8332ad9b53de566467a2b283754f
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-06-27 22:57:13 +0700

    Update data skor
    hanya menambahkan skor ke database

--- Guess the Hamster/libs/data.py
+++ Guess the Hamster/libs/data.py
@@ -2,10 +2,11 @@
 
 game_status = {
     'username': None,
-    "koin": 0,
-    "auto_catch": 0,
-    "double_point": 0,
-    "fifty_fifty": 0,
-    "is_developer": False,
-    "win_event": False
+    'koin': 0,
+    'skor':0,
+    'auto_catch': 0,
+    'double_point': 0,
+    'fifty_fifty': 0,
+    'is_developer': False,
+    'win_event': False
 }
