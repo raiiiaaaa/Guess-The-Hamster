4dfef65bbe89198a0911bc4a27662995efc3de1f
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-06-29 22:32:11 +0700

    update main loop
    syntax import function modul lebih efisien + loop main agar tidak terjadi nested loop

--- Guess the Hamster/main.py
+++ Guess the Hamster/main.py
@@ -1,28 +1,27 @@
 from libs.login import login, menu_message
-from libs import data
-from shop import hamster_shop
-from games import hamster_game
-import achievement
+from shop.hamster_shop import PowerUP_shop
+from games.hamster_game import singleplayer, vs_bot
+from achievement import achievement_collections
 
 def menu():
-    menu_message()
-    pilihan = input("Pilih menu (1-5): ")
-    if pilihan == "1":
-        print("🎮 Mode Singleplayer dipilih!\n")
-        hamster_game.singleplayer()
-    # elif pilihan == "2":
-    #     vs_bot()
-    elif pilihan == "3":
-        achievement.achievement_collections()
-    elif pilihan == "4":    
-        print("🛒 Selamat datang di PowerUP Shop!\n")
-        hamster_shop.PowerUP_shop()
-    elif pilihan == "5":
-        print("👋 Terima kasih telah bermain!")
-        exit()
-    else:
-        print(f"Input tidak valid. Coba lagi!\n")
-        menu()
+    while True:
+        menu_message()
+        pilihan = input("Pilih menu (1-5): ")
+        if pilihan == "1":
+            print("🎮 Mode Singleplayer dipilih!\n")
+            singleplayer()
+        elif pilihan == "2":
+            vs_bot()
+        elif pilihan == "3":
+            achievement_collections()
+        elif pilihan == "4":    
+            print("🛒 Selamat datang di PowerUP Shop!\n")
+            PowerUP_shop()
+        elif pilihan == "5":
+            print("👋 Terima kasih telah bermain!")
+            break
+        else:
+            print(f"Input tidak valid. Coba lagi!\n")
 
 def main():
     login()
