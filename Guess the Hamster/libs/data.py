8dc28b40f08fe46d4abb1d56f2bfa157935aadad
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-06-29 22:53:22 +0700

    update data database
    pemindahan database dari file login.py ke data.py

--- Guess the Hamster/libs/data.py
+++ Guess the Hamster/libs/data.py
@@ -1,11 +1,17 @@
-# libs/data.py
-
+# status player
 game_status = {
     'username': None,
     "koin": 0,
     "auto_catch": 0,
     "double_point": 0,
     "fifty_fifty": 0,
+    '5lose_streak': False,
     "is_developer": False,
     "win_event": False
+}
+
+# Database akun
+database = {
+    "Rayyuuu": "SevenHamster",
+    "Jojo": "ParnoMusic"
 }
