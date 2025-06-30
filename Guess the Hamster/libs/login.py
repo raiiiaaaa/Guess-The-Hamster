54407fc49d8fc4c38a59d7b605330191268f0296
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-06-30 11:42:15 +0700

    update login achievement
    menambahkan notif achievement developer

--- Guess the Hamster/libs/login.py
+++ Guess the Hamster/libs/login.py
@@ -6,20 +6,22 @@ def login():
     while True:
         kesempatan = 3
         while kesempatan > 0:
-            print('===== LOGIN =====')
+            print('========== LOGIN ==========')
             username = input("Masukkan username: ")
             password = input("Masukkan password: ")
+            print('========================================')
 
             if username in data.database and data.database[username] == password:
                 print("Login berhasil! Selamat datang,", username)
                 if username == 'Rayyuuu':
                     print('Anda login sebagai developer!')
                     data.game_status['is_developer'] = True
+                    print('>>>Kamu mendapatkan achievement! 🙌👑 A DEVELOPER❗❗❓❓<<<')
                 data.game_status['username'] = username
                 return
             else:
                 kesempatan -= 1
-                print(f"Username atau password salah. Sisa kesempatan: {kesempatan}\n")
+                print(f"Username atau password salah. Sisa kesempatan: {kesempatan}\n\n")
 
         print("Login gagal 3 kali. Program dikunci selama 60 detik...\n")
         for i in range(60, 0, -1):
