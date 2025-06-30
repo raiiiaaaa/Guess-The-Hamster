62eb98b3e913a73f5930f455163d914032e2de33
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-07-01 00:12:11 +0700

    update main menu
    menghapus menu vs bot

--- Guess the Hamster/main.py
+++ Guess the Hamster/main.py
@@ -1,23 +1,21 @@
 from libs.login import login, menu_message
 from shop.hamster_shop import PowerUP_shop
-from games.hamster_game import singleplayer, vs_bot
+from games.hamster_game import play_game
 from achievement import achievement_collections
 
 def menu():
     while True:
         menu_message()
-        pilihan = input("Pilih menu (1-5): ")
+        pilihan = input("Pilih menu (1-4): ")
         if pilihan == "1":
-            print("🎮 Mode Singleplayer dipilih!\n")
-            singleplayer()
+            print("🎮 Selamat datang di Game!!\n")
+            play_game()
         elif pilihan == "2":
-            vs_bot()
-        elif pilihan == "3":
             achievement_collections()
-        elif pilihan == "4":    
+        elif pilihan == "3":    
             print("🛒 Selamat datang di PowerUP Shop!\n")
             PowerUP_shop()
-        elif pilihan == "5":
+        elif pilihan == "4":
             print("👋 Terima kasih telah bermain!")
             break
         else:
